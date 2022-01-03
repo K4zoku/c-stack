5bb396bd1cac2b5fea64f99105f7cd579bbb2bc8
K4zoku <kazoku0356@gmail.com>
2022-01-03 23:37:30 +0700

    Initial commit

--- test.c
+++ test.c
@@ -2,19 +2,24 @@
 #include <stdio.h>
 #include "cstack.h"
 
-int main() {
-    const char original[] = "abcdefgh";
+int main(int argc, char ** argv) {
+    (void) argc;
+    (void) argv;
+    const char str[] = "abcdefgh";
     cstack_type(char) rev = cstack_new();
     size_t i;
-    for (i = 0; original[i] != '\0'; ++i) {
-        cstack_push(rev, original[i]);
+    for (i = 0; str[i] != '\0'; ++i) {
+        cstack_push(rev, str[i]);
     }
+
+    printf("size: %u\n", cstack_size(rev));
 
     while (!cstack_empty(rev)) {
         putchar(cstack_top(rev));
         cstack_pop(rev);
     }
     putchar('\n');
+
     cstack_free(rev);
     return 0;
 }
